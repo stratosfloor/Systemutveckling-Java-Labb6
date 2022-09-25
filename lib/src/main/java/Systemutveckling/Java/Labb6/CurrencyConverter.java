a7b45e79ba50944051704fffc08343cd664a1a97
Emil Noren <stratosfloor@hotmail.com>
2022-09-25 19:31:42 +0200

    Add try/catch for nullpointerexception to catch conversion to same currency

--- lib/src/main/java/Systemutveckling/Java/Labb6/CurrencyConverter.java
+++ lib/src/main/java/Systemutveckling/Java/Labb6/CurrencyConverter.java
@@ -6,51 +6,55 @@ import java.math.RoundingMode;
 import org.joda.money.*;
 
 public class CurrencyConverter {
-	
-	
+
 	private static final BigDecimal USDtoEUR = new BigDecimal("1.0250677");
 	private static final BigDecimal USDtoGBP = new BigDecimal("0.90171468");
-	
+
 	private static final BigDecimal EURtoGBP = new BigDecimal("0.88150738");
 	private static final BigDecimal EURtoUSD = new BigDecimal("0.97554528");
-	
+
 	private static final BigDecimal GBPtoUSD = new BigDecimal("1.1089982");
 	private static final BigDecimal GBPtoEUR = new BigDecimal("1.1344205");
-	
+
 	private static final CurrencyUnit usd = CurrencyUnit.of("USD");
 	private static final CurrencyUnit eur = CurrencyUnit.of("EUR");
 	private static final CurrencyUnit gbp = CurrencyUnit.of("GBP");
-	
+
 	protected Money convert(Money amount, CurrencyUnit fromUnit, CurrencyUnit toUnit) {
 
 		BigDecimal rate = null;
-		
-		
-		if(fromUnit == usd) {
-			if (toUnit == gbp) {
-				rate = USDtoGBP;
-			} else if (toUnit == eur) {
-				rate = USDtoEUR;
+
+
+		try {
+
+			if (fromUnit == usd) {
+				if (toUnit == gbp) {
+					rate = USDtoGBP;
+				} else if (toUnit == eur) {
+					rate = USDtoEUR;
+				}
+			} else if (fromUnit == eur) {
+				if (toUnit == gbp) {
+					rate = EURtoGBP;
+				} else if (toUnit == usd) {
+					rate = EURtoUSD;
+				}
+
+			} else if (fromUnit == gbp) {
+				if (toUnit == usd) {
+					rate = GBPtoUSD;
+				} else if (toUnit == eur) {
+					rate = GBPtoEUR;
+				}
+			} else {
+				System.out.println("FEL");
 			}
-		} else if (fromUnit == eur) {
-			if (toUnit == gbp) {
-				rate = EURtoGBP;
-			} else if (toUnit == usd) {
-				rate = EURtoUSD;
-			}
-			
-		} else if (fromUnit == gbp) {
-			if (toUnit == usd) {
-				rate = GBPtoUSD;
-			} else if (toUnit == eur) {
-				rate = GBPtoEUR;
-			}
-		} else {
-			System.out.println("FEL");
+
+		} catch (NullPointerException e) {
+			System.out.println("Cannot convert to the same currency");
 		}
-	
 		return amount.convertedTo(toUnit, rate, RoundingMode.HALF_UP);
-		
+
 	}
 
 }
